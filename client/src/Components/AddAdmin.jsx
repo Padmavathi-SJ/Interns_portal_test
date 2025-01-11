83eb94c9d5120f07933d1e899c19e67d88a360db
Padmavathi SJ <149775301+Padmavathi-SJ@users.noreply.github.com>
2025-01-11 23:24:27 +0530

    Update AddAdmin.jsx

--- client/src/Components/AddAdmin.jsx
+++ client/src/Components/AddAdmin.jsx
@@ -1,4 +1,6 @@
-
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AddAdmin = () => {
   const [email, setEmail] = useState('');
