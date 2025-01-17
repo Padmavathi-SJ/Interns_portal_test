e9729068874710d11217a4a081e689b64067b98f
Padmavathi SJ <149775301+Padmavathi-SJ@users.noreply.github.com>
2025-01-17 22:45:54 +0530

    Update EmployeeHome.jsx

--- client/src/Components/USER/Dashboard-Metrics/EmployeeHome.jsx
+++ client/src/Components/USER/Dashboard-Metrics/EmployeeHome.jsx
@@ -8,6 +8,9 @@ const EmployeeHome = () => {
   return (
     <div className="flex flex-wrap justify-between p-4 gap-5">
       {/* Profile */}
+<div className="bg-white dark:bg-gray-800 shadow-lg border border-gray-300 dark:border-gray-700 rounded-lg p-4 flex-1 max-w-lg h-full">
+        <Profile />
+      </div>
       
       {/* Leave Dashboard and Team Dashboard */}
       <div className="bg-white dark:bg-gray-800 shadow-md border border-gray-300 dark:border-gray-700 rounded-lg p-4 flex-1 max-w-lg h-full flex flex-col justify-between">
