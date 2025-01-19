20bb3c745520211aab7fcf9b1c4d1a136a84bdfe
Padmavathi SJ <149775301+Padmavathi-SJ@users.noreply.github.com>
2025-01-19 22:45:32 +0530

    Updated

--- server/Routes/EmployeeRoutes.js
+++ server/Routes/EmployeeRoutes.js
@@ -1,8 +1,4 @@
-import express from "express";
-import connection from "../DB/db.js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"; 
-import dotenv from "dotenv";
+
 
 dotenv.config();
 
