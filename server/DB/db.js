6137afb920a2f11b417eea235223dfdd805525a3
Padmavathi-SJ <padmasj54@gmail.com>
2025-04-22 19:25:40 +0530

    dev dependency updated

--- server/DB/db.js
+++ server/DB/db.js
@@ -2,17 +2,21 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-const pool = mysql.createPool(urlDB);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306,
+});
 
 pool.getConnection((err, connection) => {
-    if (err) {
-        console.log("database connection error", err.message);
-    } else {
-        console.log("database connected successfully");
-        connection.release();
-    }
+  if (err) {
+    console.log("Database connection error:", err.message);
+  } else {
+    console.log("Database connected successfully");
+    connection.release();
+  }
 });
 
 export default pool;
